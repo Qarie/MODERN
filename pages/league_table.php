aaafca6322a3e4bbdd47f8cb55b71936bf6b843b
Qarie <ceerprotocol@gmail.com>
2023-01-19 17:04:02 +0300

    league table

--- pages/league_table.php
+++ pages/league_table.php
@@ -58,131 +58,26 @@ if (!(isset($_SESSION["login"]) && $_SESSION["login"] == "OK")) {
 
                                     </div>
 
-                                    <table class="table mb-0">
-
-                                        <tr>
-
-
-
-                                            <td>
-                                                <a data-bs-toggle="modal" data-bs-target="#modalId<?= $result['id']; ?>" class="btn btn-warning"><i class="las la-eye"></i></a>
 
 
 
 
-                                                <!-- Modal Body -->
-                                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                                <div class="modal fade" id="modalId<?= $result['id']; ?>" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-                                                    <div class="modal-dialog modal-lg" role="document">
-                                                        <div class="modal-content">
-                                                            <?php
-                                                            $seasonId = $result['id'];
-                                                            $query = mysqli_query($con, "SELECT * FROM seasons where id = $seasonId ");
-                                                            $row = mysqli_fetch_array($query);
-
-                                                            ?>
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="modalTitleId"><?= $row['tournament']; ?></h5>
-                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <table class="table">
-                                                                    <thead style="text-transform: uppercase; background: grey; color: white;">
-                                                                        <th>#</th>
-                                                                        <th>Team</th>
-                                                                        <th>p</th>
-                                                                        <th>w</th>
-                                                                        <th>d</th>
-                                                                        <th>l</th>
-                                                                        <th>gf</th>
-                                                                        <th>ga</th>
-                                                                        <th>gd</th>
-                                                                        <th>pts</th>
-
-
-
-
-                                                                    </thead>
-                                                                    <tbody style="text-transform: uppercase; font-size:11px;">
-                                                                        <?php
-                                                                        $status = '1';
-                                                                        $league = $row['tournament'];
-                                                                        $season = $row['season'];
-                                                                        $sql1 = mysqli_query($con, "select * from matches where league = '$league' and season ='$season' ");
-                                                                        $result = mysqli_fetch_array($sql1);
-                                                                        // $played = $result['status'];
-                                                                        $home_score = $result['home_pts'];
-                                                                        $away_score = $result['away_pts'];
-                                                                        $win = 0;
-                                                                        $draw = 0;
-                                                                        $lose = 0;
-
-                                                                        $w = 3;
-                                                                        $d = 1;
-                                                                        $l = 0;
-
-
-                                                                        // $data = array();
-                                                                        // $data = $row['teams'];
-                                                                        // $sp_data = explode(",", $data);
-                                                                        // $count = 0;
-
-
-                                                                        $pts = ($w * $win) + ($d * $draw) + ($l * $lose);
-                                                                        $count = 0;
-
-                                                                        $teams = mysqli_query($con, "select * from registered_teams where season = $seasonId");
-                                                                        foreach ($teams as $team) {
-                                                                            $count++;;
-                                                                        ?>
-
-                                                                            <tr>
-                                                                                <td><?= $count; ?></td>
-                                                                                <td><?= $team['team']; ?></td>
-                                                                                <td>
-                                                                                    <?php
-                                                                                    $id = $team['id'];
-
-                                                                                    $results = mysqli_query($con, "select * from matches where league = '$league' and season ='$season' and home_team = 'ARUA HILL SC' ");
-                                                                                    foreach ($results as $result) {
-                                                                                        $played = $result['status'];
-                                                                                    ?>
-<?php echo $played;?>
-                                                                                    <?php
-                                                                                    } ?>
-                                                                                </td>
-                                                                                <td><?= $win; ?></td>
-                                                                                <td><?= $draw; ?></td>
-                                                                                <td><?= $lose; ?></td>
-                                                                                <td><? ?></td>
-                                                                                <td><? ?></td>
-                                                                                <td><? ?></td>
-                                                                                <td><?= $pts; ?></td>
-                                                                            </tr>
-
-
-                                                                        <?php
-                                                                        } ?>
-
-
-                                                                    </tbody>
-                                                                </table>
-                                                            </div>
-
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-
-                                            </td>
+                                    <div>
+                                        <a href="view.php?id=<?= $result['id']; ?>" class="btn btn-warning">VIEW</a>
 
 
 
 
 
-                                        </tr>
+                                    </div>
 
-                                    </table>
+
+
+
+
+
+
+
 
                                 </div>
                             </div>
